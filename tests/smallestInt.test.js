244f4d2be735f724d6cb333cbf55b937cbe9e65d
Heba Omar <74996096+Heba-WebDev@users.noreply.github.com>
2023-03-15 03:42:38 +0300

    Refactored the tests

--- tests/smallestInt.test.js
+++ tests/smallestInt.test.js
@@ -2,27 +2,22 @@ import { it, describe, expect } from "vitest";
 import smallestInt from "./smallestInt";
 
 
-
 describe('smallestInt', () => {
-    it('should be a function', () => {
-        expect(typeof smallestInt).toBe('function');
-    })
-})
-describe('smallestInt', () => {
-    it('should throw an error if the argument is not an array', (arr) => {
+    
+     it('should throw an error if the argument is not an array', () => {
         expect(() => smallestInt("string")).toThrow("Not a valid argument")
     })
-})
-
-describe('smallestInt', () => {
-    it('should be return smallest number in an array', () => {
+    
+    
+     it('should be return smallest number in an array', () => {
         expect(smallestInt([2,4,5,88,0,1])).toBe(0);
     })
-})
-
-
-describe('smallestInt', () => {
+    
+    
     it('should be return smallest number in an array', () => {
         expect(smallestInt([5,6,90,123,657,4])).toBe(4);
     })
 })
+
+
+
